1961d4a27dbeb48c2b186ee1ba9afd9043373677
daivydking <you@example.com>
2023-08-15 12:16:08 -0700

    function c programming

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if lowercase alphabet and 0 otherwise
+ * @c: contains value to be compared
+ * Return: Always 0.
+ */
+int _islower(int c)
+{
+
+if (c > 'a' && c < 'z')
+{
+
+return (1);
+}
+
+else
+{
+
+return (0);
+}
+
+}
